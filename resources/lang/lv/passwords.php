fb472aa8067473a9503f22ff87a7b862a249db2e
Mārtiņš Usāns <Mrti Usns>
2017-05-04 04:09:31 +0300

    add lv lang

---
+++ resources/lang/lv/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Parolei jāsastāv no vismaz sešiem simboliem un jāsaktrīt ar apstiprinājumu.',
+    'reset' => 'Jūsu parole ir atiestatīta!',
+    'sent' => 'Mēs jums nosūtījām e-pastu ar saiti uz paroles atiestatīšanu!',
+    'token' => 'Šī paroles atiestatīšanas pazīšanas zīme (token) ir nepareiza.',
+    'user' => "Mēs nevaram atrast lietotāju ar šādu e-pasta adresi.",
+
+];
